4be1f80103aed78ab20d095d8a7875c3d79e31e5
Jayjeet1013 <jayjeet1013@gmail.com>
2023-09-28 23:06:30 +0530

    donne

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+// import { MartianWallet } from '@martianwallet/aptos-wallet-adapter';
+
+
+
+const wallets = [new PetraWallet()];
+// const wallets = [new MartianWallet()];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+  <App />
+</AptosWalletAdapterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
